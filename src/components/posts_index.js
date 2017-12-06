2dc4f499612e8e468b1c6a6dee989b2883590f90
John Winslow <winslow.johnm@gmail.com>
2017-12-05 19:05:09 -0800

    Connect fetchposts ajax call to postsindex component

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions';
 
 class PostsIndex extends Component {
+  componentDidMount() {
+    this.props.fetchPosts();
+  }
+
   render() {
     return (
       <div>
-      Posts Index
+        Posts Index
       </div>
     );
   }
 }
 
-export default PostsIndex;
+export default connect(null, { fetchPosts })(PostsIndex);
